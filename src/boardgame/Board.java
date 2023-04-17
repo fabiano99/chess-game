d072366ed0d5aa42e10845cb5b3fd576556c5f8f
Fabiano Moreira <fabiano96@hotmail.com>
2023-04-17 19:59:04 -0300

    pawn

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -36,7 +36,7 @@ public class Board {
 	}
 	
 	public void placePiece(Piece piece, Position position) {
-		if (thereIsAPices(position)) {
+		if (thereIsAPiece(position)) {
 			throw new BoardException("There is already a piece on position " + position);
 		}
 		pieces[position.getRow()][position.getColumn()] = piece;
@@ -64,7 +64,7 @@ public class Board {
 		return positionExists(position.getRow(), position.getColumn());
 	}
 	
-	public boolean thereIsAPices(Position position) {
+	public boolean thereIsAPiece(Position position) {
 		if (!positionExists(position)) {
 			throw new BoardException("Position not on the board");
 		}
